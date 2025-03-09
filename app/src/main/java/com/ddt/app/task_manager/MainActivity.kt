f0a6a8a9b87ce8831119d73822850ce8b42b6d98
kabiraaaa <piptech.kabir@gmail.com>
2025-03-09 07:12:08 +0530

    Main Activity updated

--- app/src/main/java/com/ddt/app/task_manager/MainActivity.kt
+++ app/src/main/java/com/ddt/app/task_manager/MainActivity.kt
@@ -1,20 +1,100 @@
 package com.ddt.app.task_manager
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ddt.app.task_manager.databinding.ActivityMainBinding
+import com.ddt.app.task_manager.domain.ScheduleWorkManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private val sharedPreferences by lazy {
+        getSharedPreferences(
+            "app_prefs",
+            Context.MODE_PRIVATE
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initDailyNoti()
+        setUpNavigation()
+        applySavedTheme()
+    }
+
+    private fun initDailyNoti() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this, android.Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                    REQUEST_NOTIFICATION_PERMISSION
+                )
+            } else {
+                ScheduleWorkManager.scheduleDailyTaskReminder(this)
+            }
+        } else {
+            ScheduleWorkManager.scheduleDailyTaskReminder(this)
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ScheduleWorkManager.scheduleDailyTaskReminder(this)
+            } else {
+                Toast.makeText(this, "Notification permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun setUpNavigation() {
+        val navView: BottomNavigationView = binding.bottomNavigationBar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
+        bottomNavItemChangeListener(navView)
+    }
+
+    private fun bottomNavItemChangeListener(navView: BottomNavigationView) {
+        navView.setOnItemSelectedListener { item ->
+            if (item.itemId != navView.selectedItemId) {
+                navController.popBackStack(item.itemId, inclusive = true, saveState = false)
+                navController.navigate(item.itemId)
+            }
+            true
+        }
+    }
+
+    private fun applySavedTheme() {
+        val savedColor = sharedPreferences.getString("primary_color", "#756EF3")
+        window.statusBarColor = Color.parseColor(savedColor)
+    }
+
+    companion object {
+        private const val REQUEST_NOTIFICATION_PERMISSION = 1001
     }
 }
